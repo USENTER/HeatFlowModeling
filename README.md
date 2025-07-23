0bc8460394e4c5fb06d5a4c9a04b593355df9592
shovan <shovan.jagadev@gmail.com>
2025-07-23 13:59:17 -0700

    updated README, and improved parallelization

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HeatFlowModeling
+
+### Overview ###
+Initially created for a class, Nanomanufacturing for Sustainability, I built on the code to allow for simulating multiple materials in parallel. The parallelization is achieved through MATLAB's Parallel Computing Toolbox. 
+
+The code itself numerically solves a system of differential equations to calculate the temperature a film of material will settle at given a set of conditions like solar irradiance, the material's properties, the ambient temperature, etc.  
+
+Below is a picture of the equations used.
--- heatFlowModeling.m
+++ heatFlowModeling.m
@@ -10,74 +10,95 @@ wavl_arr = linspace(wavl_start,wavl_end,num);
 w_step=wavl_arr(2)-wavl_arr(1);
 tau_full = interp1(wavl_atm, t_atm, wavl_arr, 'linear');
 %% parameters
-IR_emis = 1; % emittance of the object.
-h = 12; % conduction and convection, W/m2
 Tamb = 30 + 273; % Ambient temperature
 I_ES_arr = [0 200 400 600 800 1000]; % Added other I_ES values to solar array
 R_solar_arr = linspace(0, 1, 20); % Changed to linspace reflectance for the solar 
-%% Calculation
-detT_arr=zeros(length(I_ES_arr),length(R_solar_arr));
-% 'detT_arr' is to store the temperature drop.
-for ii=1:length(I_ES_arr)
-I_ES= I_ES_arr(ii);
-for jj=1:length(R_solar_arr)
-R_solar = R_solar_arr(jj);
-Tobj = Tamb;
-% below is calculation
-if Prad(Tobj) > P_cdcv(Tamb,Tobj) + Psun_abs() + Pamb(Tamb)
-while Prad(Tobj)>P_cdcv(Tamb,Tobj)+Psun_abs()+Pamb(Tamb)
-Tobj=Tobj-0.1;
-end
-else
-while Prad(Tobj) < P_cdcv(Tamb,Tobj)+Psun_abs()+Pamb(Tamb)
-Tobj=Tobj+0.1;
-end
-end
-detT_arr(ii,jj)=Tamb-Tobj; % record the difference
-end
-end
-%% plot temperature drop versus T_amb
-figure()
-for i = 1:size(detT_arr, 1)
-plot(R_solar_arr, detT_arr(i,:), 'o-', 'LineWidth', 2);
-hold on;
-end
-xlabel('\rho_{sun}');
-ylabel('Temperature drop, detT [C]')
-title('Temperature drop vs. Solar Reflectance')
-legend(compose("I_{ES}=%d W/m^2", I_ES_arr));
 
+% Define random example materials
+materials(1).name = 'Aluminum';
+materials(1).IR_emis = 0.05;
+materials(1).h = 15;
+
+materials(2).name = 'Glass';
+materials(2).IR_emis = 0.9;
+materials(2).h = 10;
+
+materials(3).name = 'Ceramic';
+materials(3).IR_emis = 0.7;
+materials(3).h = 8;
+
+materials(4).name = 'Plastic';
+materials(4).IR_emis = 0.95;
+materials(4).h = 12;
+
+numMaterials = length(materials);
+results = cell(numMaterials,1);
+
+parfor m = 1:numMaterials
+    mat = materials(m);
+    IR_emis = mat.IR_emis;
+    h = mat.h;
+    detT_arr=zeros(length(I_ES_arr),length(R_solar_arr));
+    for ii=1:length(I_ES_arr)
+        I_ES= I_ES_arr(ii);
+        for jj=1:length(R_solar_arr)
+            R_solar = R_solar_arr(jj);
+            Tobj = Tamb;
+            % below is calculation
+            if Prad(Tobj, IR_emis) > P_cdcv(Tamb,Tobj,h) + Psun_abs(R_solar,I_ES) + Pamb(Tamb, IR_emis)
+                while Prad(Tobj, IR_emis)>P_cdcv(Tamb,Tobj,h)+Psun_abs(R_solar,I_ES)+Pamb(Tamb, IR_emis)
+                    Tobj=Tobj-0.1;
+                end
+            else
+                while Prad(Tobj, IR_emis) < P_cdcv(Tamb,Tobj,h)+Psun_abs(R_solar,I_ES)+Pamb(Tamb, IR_emis)
+                    Tobj=Tobj+0.1;
+                end
+            end
+            detT_arr(ii,jj)=Tamb-Tobj; % record the difference
+        end
+    end
+    results{m}.detT_arr = detT_arr;
+    results{m}.name = mat.name;
+end
+
+%% plot temperature drop versus T_amb for each material
+figure()
+for m = 1:numMaterials
+    detT_arr = results{m}.detT_arr;
+    subplot(2,2,m)
+    for i = 1:size(detT_arr, 1)
+        plot(R_solar_arr, detT_arr(i,:), 'o-', 'LineWidth', 2);
+        hold on;
+    end
+    xlabel('\rho_{sun}');
+    ylabel('Temperature drop, detT [C]')
+    title(['Material: ' results{m}.name])
+    legend(compose('I_{ES}=%d W/m^2', I_ES_arr));
+end
 
 %% DIY functions
 function y=Ibb(wavl_ARR,T) % spectral.
-% spectral hemisphere emissive power of a blackbody
-C1=3.742e8/pi; % C1 unit: W.um^4.m^-2
-C2= 1.439e4;
-y=C1./((wavl_ARR.^5).*(exp(C2./(wavl_ARR.*T))-1));
+    % spectral hemisphere emissive power of a blackbody
+    C1=3.742e8/pi; % C1 unit: W.um^4.m^-2
+    C2= 1.439e4;
+    y=C1./((wavl_ARR.^5).*(exp(C2./(wavl_ARR.*T))-1));
 end
-function y = Prad(Tsample)
-% input ambient temperautre, return radiation from the ambient.
-y = pi*w_step*sum(IR_emis*Ibb(wavl_arr,Tsample));
+function y = Prad(Tsample, IR_emis)
+    y = pi*w_step*sum(IR_emis*Ibb(wavl_arr,Tsample));
 end
-function y = P_cdcv(T_env,T_film)
-% input ambient temperautre, return radiation from the ambient.
-y = h*(T_env - T_film);
+function y = P_cdcv(T_env,T_film,h)
+    y = h*(T_env - T_film);
 end
-function y = Psun_abs()
-% input ambient temperautre, return radiation from the ambient.
-y = (1-R_solar)*I_ES;
+function y = Psun_abs(R_solar,I_ES)
+    y = (1-R_solar)*I_ES;
 end
-function y = Pamb(T)
-% input ambient temperautre, return radiation from the ambient.
-detP=0.01;
-P_amb=0;
-for p=0.01:detP:0.99 % p = cos(theta)
-t = (1-tau_full.^(1/p));
-% t: atmospherical transmittance w.r.t. wavl and angle
-% below is to transfer integral into Riemann sum
-TempValue=2*pi*detP*p*IR_emis*(w_step*sum(t.*Ibb(wavl_arr,T)));
-P_amb = P_amb + TempValue;
-end
-y=P_amb;
+function y = Pamb(T, IR_emis)
+    detP=0.01;
+    p=0.01:detP:0.99; % p = cos(theta)
+    t = (1-tau_full.^(1./p)); % size: [numel(wavl_arr), numel(p)]
+    Ibb_vals = Ibb(wavl_arr,T); % size: [numel(wavl_arr), 1]
+    TempValue = 2*pi*detP*p.*IR_emis.*(w_step*sum(t.*Ibb_vals,1)); % sum over wavl_arr, keep p
+    P_amb = sum(TempValue);
+    y=P_amb;
 end
 end
